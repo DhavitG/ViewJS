e092f65c6370669519d605bed575df952e90bbee
Dhavit Gandhi <gdhavit@gmail.com>
2025-07-09 18:01:37 +0530

    Router setup

--- ViewJS/src/App.jsx
+++ ViewJS/src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import MovieCard from './Components/MovieCard'
 import Home from './Pages/Home'
+import Favorites from './Pages/Favorites'
+import {Routes, Route} from "react-router-dom"
 
 function App() {
 
   return (
-    <div>
-      <Home/>
-    </div>
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/favorites' element={<Favorites/>}/>
+      </Routes>
+    </main>
   )
 }
 
