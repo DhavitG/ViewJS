e47abfee20aba1070f9045b3d85b542ffabcec2e
Dhavit Gandhi <gdhavit@gmail.com>
2025-07-09 18:13:57 +0530

    Navbar and styling

--- ViewJS/src/App.jsx
+++ ViewJS/src/App.jsx
@@ -1,19 +1,22 @@
-import './App.css'
-import MovieCard from './Components/MovieCard'
-import Home from './Pages/Home'
-import Favorites from './Pages/Favorites'
-import {Routes, Route} from "react-router-dom"
+import "./css/App.css";
+import MovieCard from "./Components/MovieCard";
+import Home from "./Pages/Home";
+import Favorites from "./Pages/Favorites";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 
 function App() {
-
   return (
-    <main className='main-content'>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/favorites' element={<Favorites/>}/>
-      </Routes>
-    </main>
-  )
+    <div>
+      <Navbar/>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
